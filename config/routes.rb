ff4211680acfa483a8d9124ec0ba05b177c973fa
Timothy Kibet <tkkibet13@gmail.com>
2022-11-24 19:02:24 +0300

    Complete Assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get '/cheeses', to: 'cheeses#index'
+#  get '/cheeses/1', to: "cheeses#first"
+#  get '/cheeses/2', to: "cheeses#second"
+#  get '/cheeses/3', to: "cheeses#third"
+  get '/cheeses/:id', to: 'cheeses#show'
 end
